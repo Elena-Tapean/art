b4efb50ed153d14c2549a23d823536c4719edf36
Elena <elena_tapean@yahoo.com>
2022-11-24 12:19:40 +0200

    wrote article and moving background css

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
   <div id="app">
-    <NavBar />
     <router-view/>
-    <FooterBar />
   </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue';
-import FooterBar from './components/FooterBar.vue';
-
 export default {
-  name: 'App',
-  components: {
-    NavBar,
-    FooterBar
-  }
+  name: 'App'
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
